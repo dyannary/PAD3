3c250ce3281ab8b9687757d3c8153e6d1cdc22b5
Diana <diana.ribac001@gmail.com>
2023-12-11 18:13:06 +0200

    add files

---
+++ material-react-app/src/layouts/energy-prediction/index.js
@@ -0,0 +1,313 @@
+import { useEffect, useState, useReducer } from "react";
+import axios from "axios";
+import MDBox from "components/MDBox";
+import Footer from "examples/Footer";
+
+
+import Grid from "@mui/material/Grid";
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+import MDInput from "components/MDInput";
+
+
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import MDButton from "components/MDButton";
+
+import Temp from "./temp";
+import Battery from "./battery";
+import Dial from "./dial";
+import AccelDial from "./accelDial";
+
+import mqtt from "mqtt";
+import { options } from "../../config/mqtt.config";
+import { host } from "../../config/mqtt.config";
+import MDTypography from "components/MDTypography";
+
+import { Chart } from "react-google-charts"
+
+import SolarPowerIcon from '@mui/icons-material/SolarPower';
+
+
+const energyPrediction = () => {
+
+
+  const [messages, setMessages] = useState([]);
+
+  //  console.log(messages);
+
+  async function getMessages() {
+    try {
+      const response = await axios.post('http://localhost:3001/api/messages/getBySensorId', { "sensor_id": 1 });
+      let result = response.data;
+      console.log(result.length);
+
+      let shortResult = result.splice(result.length - 50, result.length);
+
+      setMessages({
+        labels: shortResult.map(x => x.message_id),
+        datasets: { label: "Temperature", data: shortResult.map(x => JSON.parse(x.message).temp) },
+      });
+
+      console.log(messages);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  /*   Set Data */
+  const [settings, setSettings] = useState(null);
+
+  const changeHandler = (e) => {
+    setSettings(e.target.value);
+  };
+
+
+
+  /* MQTT */
+  const [client, setClient] = useState(null);
+  const [connectStatus, setConnectStatus] = useState(null);
+
+  const [temp, setTemp] = useState(0);
+  const [hum, setHum] = useState(0);
+
+  const data = [
+    [
+"Courses",
+"Mathematics",
+"English",
+"Chemistry",
+"Physics",
+"Biology",
+"Average"
+    ],
+    ["Sam", 18, 10, 12, 16, 5, 12.2],
+    ["Jessica", 13, 11, 5, 18, 2, 9.8],
+    ["Suka", 15, 11, 7, 18, 9, 10],
+    ["Isaac", 19, 11, 6, 8, 5, 9.8],
+    ["Dee", 12, 11, 9, 16, 6, 10.8]
+];
+const options =  {
+  "title": "Test score in a classroom between 5 students",
+  "vAxis": { "title": "Grade" },
+  "hAxis": { "title": "Students" },
+  "seriesType": "bars",
+  "series": { "5": { "type": "line" } }
+       };
+
+  const tempTopic = 'agrobot/sensors/temperature/sensor-1';
+  const humTopic = 'agrobot/sensors/temperature/sensor-2';
+
+
+  const mqttConnect = () => {
+    setConnectStatus('Connecting');
+    let client = mqtt.connect(host, options);
+    setClient(client);
+  };
+
+  useEffect(() => {
+    mqttConnect();
+  }, []);
+
+  useEffect(() => {
+    if (client) {
+      console.log(client);
+      client.on('connect', () => {
+        setConnectStatus('Connected');
+
+        client.subscribe(tempTopic);
+        client.subscribe(humTopic);
+      });
+      client.on('error', (err) => {
+        console.error('Connection error: ', err);
+        client.end();
+      });
+      client.on('reconnect', () => {
+        setConnectStatus('Reconnecting');
+      });
+      client.on('message', (topic, message) => {
+        setConnectStatus('Message received');
+
+        if (topic === tempTopic) {
+          setTemp(JSON.parse(message.toString()).temp);
+        } else if (topic === humTopic) {
+          setHum(JSON.parse(message.toString()).hum);
+        }
+        console.log(message.toString());
+      });
+    }
+  }, [client]);
+
+
+  return (
+    <DashboardLayout marginLeft={274}>
+      <DashboardNavbar />
+      <MDBox py={3}>
+      <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="dark"
+                icon = "SolarPowerIcon"
+                title="Bookings"
+                count={281}
+                percentage={{
+                  color: "success",
+                  amount: "+55%",
+                  label: "than lask week",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                icon="leaderboard"
+                title="Today's Users"
+                count="2,300"
+                percentage={{
+                  color: "success",
+                  amount: "+3%",
+                  label: "than last month",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="success"
+                icon="store"
+                title="Revenue"
+                count="34k"
+                percentage={{
+                  color: "success",
+                  amount: "+1%",
+                  label: "than yesterday",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="primary"
+                icon="person_add"
+                title="Followers"
+                count="+91"
+                percentage={{
+                  color: "success",
+                  amount: "",
+                  label: "Just updated",
+                }}
+              />
+            </MDBox>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="primary"
+                icon="thermostat"
+                title="Temperature"
+                count={temp}
+                percentage={{
+                  color: "success",
+                  amount: "+1",
+                  label: "difference",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                icon="waterdropicon"
+                title="Humidity"
+                count={hum}
+                percentage={{
+                  color: "error",
+                  amount: "-1",
+                  label: "difference",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <MDButton variant="gradient" color="info" fullWidth type="submit" onClick={(e) => getMessages(e)}>
+                Load Data
+              </MDButton>
+            </MDBox>
+
+            <MDBox mb={1.5}>
+              <MDButton variant="gradient" color="info" fullWidth type="submit">
+                Set Data
+              </MDButton>
+            </MDBox>
+
+            <MDBox mb={1.5}>
+              <MDBox mb={2}>
+                <MDInput
+                  type="text"
+                  label="Settings"
+                  fullWidth
+                  value={settings}
+                  name="settings"
+                  onChange={changeHandler}
+                />
+              </MDBox>
+
+            </MDBox>
+
+          </Grid>
+
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <MDTypography > MQTT status : {connectStatus}</MDTypography>
+            </MDBox>
+            <MDBox mb={1.5}>
+              <MDTypography > Current Settings : {settings}</MDTypography>
+            </MDBox>
+
+          </Grid>
+        </Grid>
+        <MDBox mt={4.5}>
+          <Grid container spacing={3}>
+
+            <Grid item xs={12} md={12} lg={12}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="info"
+                  title="Statistics"
+                  description={
+                    <>
+                      (<strong>+25</strong>) hot weather today.
+                    </>
+                  }
+                  date="updated 4 min ago"
+                  chart={messages}
+                />
+                {/* <Chart
+                  chartType="ScatterChart"
+                  data={client}
+                  width = "100%"
+                  height= "400px"
+                /> */}
+              </MDBox>
+            </Grid>
+
+          </Grid>
+        </MDBox>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+};
+
+export default energyPrediction;
